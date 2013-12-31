4a2f413cbe0e9c0b09b46c59bff6cd403dd2f08c
AdrianBodin <bodinthornadrian@gmail.com>
2014-01-01 00:00:00 +0500

    Almost finished homepage and started on car model

---
+++ JapinoRenting.Data/Models/Car.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JapinoRenting.Data;
+
+public class Car
+{
+    [Key]
+    public int Id { get; set; }
+    
+    
+    //Car brand, for example: Toyota, Honda, etc.
+    [Required]
+    public string Brand { get; set; }
+    
+    //Car model, for example: Corolla, Civic, etc.
+    [Required]
+    public string Model { get; set; }
+    
+    //Car year of production
+    [Required]
+    public int Year { get; set; }
+    
+    //Registration number of the car
+    [Required]
+    public string RegistrationNumber { get; set; }
+    
+    //Chassis number of the car
+    [Required]
+    public string ChassisNumber { get; set; }
+    
+    //Car description
+    [Required]
+    public string Description { get; set; }
+    
+    //Engine for the car
+    [Required]
+    public Engine Engine { get; set; }
+    
+    //Price per day for renting the car
+    [Required]
+    [DataType(DataType.Currency)]
+    public decimal PricePerDay { get; set; }
+    
+    //Car image 
+    [Required]
+    public string ImageUrl { get; set; }
+}
---
+++ JapinoRenting.Data/Models/Engine.cs
@@ -0,0 +1,10 @@
+﻿namespace JapinoRenting.Data;
+
+public class Engine
+{
+    public string Name { get; set; }
+    public int HorsePower { get; set; }
+    public int Torque { get; set; }
+    public int Displacement { get; set; }
+    public int Cylinders { get; set; }
+}
